2d8ffc794d5911f62986551d744097356b4104f1
Alexandre Díaz <dev@redneboa.es>
2016-05-16 22:16:55 +0200

    Fix Icons

--- static/js/servers.js
+++ static/js/servers.js
@@ -316,12 +316,12 @@ $(function(){
 				$name.text(data['name']);
 				$gametype.text(data['gametype']);
 				
-				if (data['visible'] == 0)
-					$flags.find('.tw-no-register').addClass('fa-eye').removeClass('fa-eye-slash').css('color','#333').prop('title', $BABEL_STR_PRIVATE_SERVER);
+				if (data['register'] == 0)
+					$flags.find('.tw-no-register').addClass('fa-eye').removeClass('fa-eye-slash text-muted').prop('title', $BABEL_STR_PRIVATE_SERVER);
 				else
-					$flags.find('.tw-no-register').addClass('fa-eye-slash text-muted').removeClass('fa-eye').css('color','#BBB').prop('title', $BABEL_STR_PUBLIC_SERVER);
+					$flags.find('.tw-no-register').addClass('fa-eye-slash text-muted').removeClass('fa-eye').prop('title', $BABEL_STR_PUBLIC_SERVER);
 				
-				if (data['public'] == 0)
+				if (data['password'] == 0)
 					$flags.find('.tw-password').removeClass('text-muted').prop('title', $BABEL_STR_REGISTER_SERVER);
 				else
 					$flags.find('.tw-password').addClass('text-muted').prop('title', $BABEL_STR_NOT_REGISTER_SERVER);
